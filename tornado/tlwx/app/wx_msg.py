14eb1f95539abd5d1d749d06fa740fbff899bb74
fjli <fjli@2a503d66-d6cd-11e6-878b-9940a70e2152>
2018-04-26 02:44:20 +0000

    公众号交互回应部分实现

--- tornado/tlwx/app/wx_msg.py
+++ tornado/tlwx/app/wx_msg.py
@@ -97,14 +97,37 @@ class Custom:
     def bill_query(curr_symbol, qual_grace_bal, tot_due_amt, billing_date, pmt_due_date):
         today = datetime.date.today().strftime('%m月%d日')
         description = today + '\n账单金额：' + curr_symbol + qual_grace_bal + '\n最低还款额：'\
-           + curr_symbol + tot_due_amt + '\n账单日：' + billing_date + '\n最后还款日：' + pmt_due_date\
-           + '\n备注' + '\n' + '-' * 30 + '\n<b>阅读全文</b>'
+           + curr_symbol + tot_due_amt + '\n账单日：' + billing_date + '\n最后还款日：' + pmt_due_date
         articles = [
             {
                 'title': '我的账单',
                 'description': description,
                 'url': get_oauth_url('/ccrd/bill'),
-                #'picurl': config.BASE_URL + '/static/img/ccrd/mes_apply1.jpg',
+            },
+        ]
+        return articles
+
+    def limit_query(curr_symbol, acct_limit, acct_otb, acct_cash_otb):
+        today = datetime.date.today().strftime('%m月%d日')
+        description = today + '\n信用额度：' + curr_symbol + acct_limit + '\n可用额度：'\
+           + curr_symbol + acct_otb + '\n预借现金额度：' + acct_cash_otb
+        articles = [
+            {
+                'title': '我的额度',
+                'description': description,
+                'url': get_oauth_url('/ccrd/limit'),
+            },
+        ]
+        return articles
+
+    def point_query(point_bal, ctd_earned_points):
+        today = datetime.date.today().strftime('%m月%d日')
+        description = today + '\n积分余额：' + point_bal + '\n本期新增：' + ctd_earned_points
+        articles = [
+            {
+                'title': '我的积分',
+                'description': description,
+                'url': get_oauth_url('/ccrd/point'),
             },
         ]
         return articles
