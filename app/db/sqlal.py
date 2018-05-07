cb259f4498bfab7234d914f3709ab4cd47f88780
fjli <fjli@2a503d66-d6cd-11e6-878b-9940a70e2152>
2018-05-07 06:26:00 +0000

    测试小调整

--- app/db/sqlal.py
+++ app/db/sqlal.py
@@ -5,5 +5,5 @@ from sqlalchemy.orm import sessionmaker
 from ..config import DB2_DIALECT
 
 
-engine = create_engine(DB2_DIALECT, echo=True)
+engine = create_engine(DB2_DIALECT)
 Session = sessionmaker(bind=engine)
